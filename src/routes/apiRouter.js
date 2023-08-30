3efd6e40f748401e19425881217b81744d6864c2
powerckbestest <ivanchikoff-danila@yandex.ru>
2023-08-30 18:01:08 +0300

    createAnimal

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import Animal from '../../db/models'
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({ hello: 'world' });
 });
+
+router.post('/create', async(req, res) => {
+  try{
+    console.log(req.body);
+   await Animal.create({
+    name: req.body.name,
+    nick: req.body.nick,
+    desc: req.body.desc
+   })
+  return res.sendStatus(200)
+  }catch(err) {
+   return res.sendStatus(400)
+  }
+})
 
 export default router;
